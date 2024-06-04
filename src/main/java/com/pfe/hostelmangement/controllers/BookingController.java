10eac574c6833e849b587c762730cd3809545f35
Elma-dev <67378945+Elma-dev@users.noreply.github.com>
2024-06-04 15:56:13 +0100

    controllers

--- src/main/java/com/pfe/hostelmangement/controllers/BookingController.java
+++ src/main/java/com/pfe/hostelmangement/controllers/BookingController.java
@@ -3,6 +3,8 @@ package com.pfe.hostelmangement.controllers;
 import com.pfe.hostelmangement.dtos.BookingDto;
 import com.pfe.hostelmangement.services.imp.BookingServicesImpl;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,24 +16,26 @@ public class BookingController {
     private BookingServicesImpl bookingServices;
 
     @GetMapping("/all")
-    public List<BookingDto> allBookings(){
-        return bookingServices.findAll();
+    public ResponseEntity<List<BookingDto>> allBookings(){
+
+        return new ResponseEntity<List<BookingDto>>(bookingServices.findAll(),HttpStatus.OK);
     }
     @GetMapping("/user/{userId}")
-    public List<BookingDto> userBookings(@PathVariable Long userId){
-        return bookingServices.userBookings(userId);
+    public ResponseEntity<List<BookingDto>> userBookings(@PathVariable Long userId){
+        return new ResponseEntity<List<BookingDto>>(bookingServices.userBookings(userId),HttpStatus.OK);
     }
     @GetMapping("/delete/{id}")
-    public void delete(Long id){
+    public  ResponseEntity<Boolean> delete(Long id){
         bookingServices.delete(id);
+        return new ResponseEntity<Boolean>(true,HttpStatus.OK);
     }
     @PostMapping("/save")
-    public BookingDto save(@RequestBody BookingDto bookingDto){
-        return bookingServices.save(bookingDto);
+    public ResponseEntity<BookingDto> save(@RequestBody BookingDto bookingDto){
+        return  new ResponseEntity<>(bookingServices.save(bookingDto),HttpStatus.OK);
     }
     @PutMapping("/update")
-    public BookingDto update(@RequestBody BookingDto bookingDto){
-        return bookingServices.save(bookingDto);
+    public ResponseEntity<BookingDto> update(@RequestBody BookingDto bookingDto){
+        return new ResponseEntity<>(bookingServices.save(bookingDto),HttpStatus.OK);
     }
 
 
--- src/main/java/com/pfe/hostelmangement/controllers/HotelController.java
+++ src/main/java/com/pfe/hostelmangement/controllers/HotelController.java
@@ -3,6 +3,8 @@ package com.pfe.hostelmangement.controllers;
 import com.pfe.hostelmangement.dtos.HotelDto;
 import com.pfe.hostelmangement.services.imp.HotelServicesImpl;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,23 +16,26 @@ public class HotelController {
     private HotelServicesImpl hotelService;
 
     @GetMapping("/all")
-    public List<HotelDto> allHotels(){
-        return hotelService.findAll();
+    public ResponseEntity<List<HotelDto>> allHotels(){
+        return new ResponseEntity<>(hotelService.findAll(), HttpStatus.OK);
     }
     @GetMapping("/{id}")
-    public HotelDto findById(@PathVariable Long id){
-        return hotelService.findById(id);
+    public ResponseEntity<HotelDto> findById(@PathVariable Long id){
+
+        return new ResponseEntity<>(hotelService.findById(id),HttpStatus.OK);
     }
     @PostMapping("/save")
-    public HotelDto save(@RequestBody HotelDto hotelDto){
-        return hotelService.save(hotelDto);
+    public ResponseEntity<HotelDto> save(@RequestBody HotelDto hotelDto){
+
+        return new ResponseEntity<>(hotelService.save(hotelDto),HttpStatus.OK);
     }
     @DeleteMapping("/delete/{id}")
-    public void delete(@PathVariable Long id){
+    public ResponseEntity<Boolean> delete(@PathVariable Long id){
         hotelService.delete(id);
+        return new ResponseEntity<>(true,HttpStatus.OK);
     }
     @PutMapping("/update")
-    public HotelDto update(@RequestBody HotelDto hotelDto){
-        return hotelService.save(hotelDto);
+    public ResponseEntity<HotelDto> update(@RequestBody HotelDto hotelDto){
+        return new ResponseEntity<>(hotelService.save(hotelDto),HttpStatus.OK);
     }
 }

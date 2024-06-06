f4b95a581b211bd2eb1ec57f4f5a91398b748cc6
abderrahimjk <abderrahim.aitbouna@gmail.com>
2024-06-06 02:39:24 +0100

    add auth

---
+++ src/main/java/com/pfe/hostelmangement/security/UserDetailsServiceImpl.java
@@ -0,0 +1,33 @@
+package com.pfe.hostelmangement.security;
+
+import com.pfe.hostelmangement.entities.UserEntity;
+import com.pfe.hostelmangement.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        }
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                new ArrayList<>()
+        );
+    }
+}
